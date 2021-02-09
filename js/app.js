1e447c032ee57314c0e4b42c6b93d237aa900c82
abj95630 <abah20000@gmail.com>
2021-02-09 21:18:09 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -47,6 +47,9 @@ let number = event.target.getAttribute ('data-id')
 
 let i = null;
 let j = null;
+var tab = []; // declare an array to insert an object
+
+var sqte = 0
 
 var tblBody = document.getElementById("tableBody");
 
@@ -55,6 +58,7 @@ function itemTable(article, prix){
     // creates a balise <tr>
     var row = document.createElement("tr");
     row.className ="row"
+    var obj = {}; // We declare an object
   
     //creating all column
     // Create a <td> element and a text node, make the text
@@ -69,19 +73,22 @@ function itemTable(article, prix){
     var cellText1 = document.createTextNode(article);
     cell1.appendChild(cellText1);
     row.appendChild(cell1);
+    obj.article = cellText1.nodeValue; // We affect the value to this key
 
-    var cell2 = document.createElement("td"); // Getting thgetAttribute
+    var cell2 = document.createElement("td"); // Getting the getAttribute
     var cellText2 = document.createTextNode (prix + '€');
     cell2.appendChild(cellText2)
     row.appendChild(cell2);
+    obj.price = cellText2.nodeValue; // Same thing for the price
 
     var cell3 = document.createElement("td"); // Adding quantity under quantity category
-    var cellText3 = document.createTextNode('1');
+    var cellText3 = document.createTextNode("1");
     cell3.appendChild(cellText3);
-    row.appendChild(cell3);
+    row.appendChild(cell3); 
+    obj.qty = cellText3.nodeValue; // '' for the quantity
+    
 
     var cell4 = document.createElement("td"); // Adding a cross that will serve as a supress button
-        
     var cellText4 = document.createTextNode('❌');
 
     var deletebtn = document.createElement("span")
@@ -94,6 +101,10 @@ function itemTable(article, prix){
     row.appendChild(cell4);
 
     console.log(tblBody)
+
+    let strBasket = JSON.stringify(obj) // We stringify the object to pass it to the array
+    tab.push(strBasket); // We push in the array the stringified object
+    localStorage.setItem("panier",tab); // We pick the array
   
     // add the row to the end of the table body
     tblBody.appendChild(row);
@@ -103,12 +114,18 @@ function itemTable(article, prix){
     for (i = 0; i < cross.length; i++) {
         cross[i].addEventListener("click", deleteItem);
     }
-
+ 
 }
+
+
+localStorage.getItem("panier"); // We get the array to keep in the local storage
 
 function deleteItem(event){
     console.log('yoyoyo')
     event.target.parentNode.parentNode.remove() 
     //We are getting back 2 time to get to the class of the item to delete it
 }
+
+
+
 
